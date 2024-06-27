0a2f6ab7ab2112dff167855b01a368a017bb3adc
Prasenjit <prasenjitbhosale1234@gmail.com>
2024-06-27 16:13:22 +0530

    added half pyramid rotated 180Deg

--- pattern/halfPyramidRotated180Deg.java
+++ pattern/halfPyramidRotated180Deg.java
@@ -3,31 +3,28 @@ package com.pattern;
 public class halfPyramidRotated180Deg {
 
 	public static void main(String[] args) {
-		
+
+		// rotated half pyramid
+
 		int n = 5;
-		
-		// rotated half pyramid 
-		
-		
-		
-		// outer loop 
-		
-		for(int i=1;i<=n;i++) {
-			// inner loop  -> space print
-			
-			for(int j=1;j<=n-i;j++) {
+
+		// outer loop
+
+		for (int i = 1; i <= n; i++) {
+			// inner loop -> space print
+
+			for (int j = 1; j <= n - i; j++) {
 				System.out.print(" ");
 			}
-			
-			// inner loop  - > star print 
-			
-			for(int j=1;j<=i;j++) {
+
+			// inner loop - > star print
+
+			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
-			
+
 			System.out.println();
 		}
-		
 
 	}
 
