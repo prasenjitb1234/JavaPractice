c606209e2c58af78708ad27188c7c5c580b641c0
Prasenjit Bhosale <prasenjitbhosale1234@gmail.com>
2025-01-26 17:42:44 +0530

    Learned TryCatch exception handling

--- ExceptionHandling/mechanisms/Test.java
+++ ExceptionHandling/mechanisms/Test.java
@@ -4,18 +4,38 @@ import java.io.FileInputStream;
 
 public class Test {
 	public static void main(String[] args) {
-		
 
+		/*
+		 * try { FileInputStream fis = new FileInputStream("d:/abc.txt"); } catch
+		 * (Exception e) {
+		 * 
+		 * System.out.println(e); }
+		 * 
+		 * System.out.println(" \n Hello,  this is after catch block");
+		 * 
+		 */
 		
+//			------------------------
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		/*
+		 * try { Class.forName("com.mysql.jdbc.Driver"); } catch (Exception e) {
+		 * 
+		 * System.out.println(e); }
+		 * System.out.println(" \n Hello this is after catch block ");
+		 * 
+		 * 
+		 */
+
+		try {
+
+			int a = 100;
+			int b = 0;
+			int c = a / b;
+			System.out.println(c);
+		} catch (ArithmeticException e) {
+			System.out.println(e);
+		}
+		System.out.println("Hello , this is after exception handling");
+
 	}
 }
