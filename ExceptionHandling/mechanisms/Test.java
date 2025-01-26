88e731810a050667cd3a92b3a381ee6ec42f86b7
Prasenjit Bhosale <prasenjitbhosale1234@gmail.com>
2025-01-26 17:28:14 +0530

    exception

---
+++ ExceptionHandling/mechanisms/Test.java
@@ -0,0 +1,21 @@
+package com.ExceptionHandling.mechanisms;
+
+import java.io.FileInputStream;
+
+public class Test {
+	public static void main(String[] args) {
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
